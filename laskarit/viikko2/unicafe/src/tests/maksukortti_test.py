bc4f8091ccc84eedafcc78b17a90469cbefb41ca
Sami Männistö <sami.mannisto1@gmail.com>
2023-03-20 13:00:42 +0200

    Tests for ex6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alustetaan_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_rahan_lataaminen_kortille_kasvattaa_saldo_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo, 1100)
+
+    def test_rahan_nostaminen_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo, 900)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(50000000)
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_rahat_riittavat_nostoo_palauttaa_true(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(100))
+
+    def test_rahat_eivat_riita_nostoon_palauttaa_false(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(2193210931))
