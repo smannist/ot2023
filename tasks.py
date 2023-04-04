e672a7577e94329866bbf00b2a1726670c3d595c
Sami Männistö <sami.mannisto1@gmail.com>
2023-04-04 17:15:31 +0300

    Added invokes for app start, test, coverage

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/start_game.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
+
+@task
+def open_report(ctx):
+    ctx.run("xdg-open htmlcov/index.html")
