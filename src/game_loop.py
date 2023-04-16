5a12e021214687a9427219d37bf5db5cae95d8cf
Sami Männistö <sami.mannisto1@gmail.com>
2023-04-16 21:31:03 +0300

    Dropping blocks mechanism

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,12 +1,16 @@
 import pygame
+from config import FALL_TIME, FALL_SPEED
 
 class GameLoop:
     def __init__(self, renderer, display, block):
         self.renderer = renderer
         self.display = display
         self.clock = pygame.time.Clock()
+        self.previous_tick = pygame.time.get_ticks()
         self.current_block = block
         self.previous_block_coordinates = [(0,0), (0,0), (0,0), (0,0)]
+        self.fall_time = FALL_TIME
+        self.fall_speed = FALL_SPEED
 
     def start(self):
         while True:
@@ -17,7 +21,10 @@ class GameLoop:
             self.renderer.render_all(self.display)
 
             block_coordinates = self.current_block.shape_to_coordinates()
-            self.move_block(block_coordinates)
+
+            self._update_elapsed_time()
+            self._drop_block(block_coordinates)
+            self._block_moved(block_coordinates)
 
             pygame.display.update()
             self.clock.tick(60)
@@ -47,8 +54,21 @@ class GameLoop:
                     return False
         return True
 
-    def move_block(self, current_block_coordinates):
+    def _block_moved(self, current_block_coordinates):
         for i in range(len(current_block_coordinates)):
             x, y = current_block_coordinates[i]
             self.renderer.game_grid.grid[y][x] = self.current_block.color
             self.renderer.game_grid.reset_cell_colors(self.previous_block_coordinates, current_block_coordinates)
+
+    def _drop_block(self, current_block_coordinates):
+        if self.fall_time/1000 >= self.fall_speed:
+                self.fall_time = 0
+                self.previous_block_coordinates = self.current_block.shape_to_coordinates()
+                self.current_block.y += 1
+                self.renderer.game_grid.reset_cell_colors(self.previous_block_coordinates, current_block_coordinates)
+
+    def _update_elapsed_time(self):
+        current_tick = pygame.time.get_ticks()
+        elapsed_time = current_tick - self.previous_tick
+        self.fall_time += elapsed_time
+        self.previous_tick = current_tick
