9b9172b2dcff2ce8b5bdcc5e8d3116a610d8f7a0
Sami Männistö <sami.mannisto1@gmail.com>
2023-04-18 16:38:36 +0300

    Variable name changes

--- src/renderer.py
+++ src/renderer.py
@@ -17,14 +17,14 @@ class Renderer:
         self._render_grid(display, CENTER_X, CENTER_Y)
         self._render_grid_border(display, CENTER_X, CENTER_Y)
 
-    def _render_grid(self, display, dx, dy, block_size=30):
+    def _render_grid(self, display, center_x, center_y, block_size=30):
         for row in range(self.game_grid.grid.shape[0]):
             for col in range(self.game_grid.grid.shape[1]):
-                grid_rectangle = pygame.Rect(dx + col * block_size, \
-                                             dy + row * block_size, block_size, block_size)
+                grid_rectangle = pygame.Rect(center_x + col * block_size, \
+                                             center_y + row * block_size, block_size, block_size)
                 pygame.draw.rect(display, self.game_grid.grid[row][col], grid_rectangle)
 
-    def _render_grid_border(self, display, dx, dy, border_width=5):
-        border_rectangle = pygame.Rect(dx - border_width, dy - border_width, \
+    def _render_grid_border(self, display, center_x, center_y, border_width=5):
+        border_rectangle = pygame.Rect(center_x - border_width, center_y - border_width, \
                                        TETRIS_WIDTH + 30 + border_width, TETRIS_HEIGHT + 40)
         pygame.draw.rect(display, BACKGROUND_COLORS["black"], border_rectangle, border_width)
