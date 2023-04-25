db9e000143535cdeb76b695f76ce0859342b7868
Sami Männistö <sami.mannisto1@gmail.com>
2023-04-25 21:42:14 +0300

    Lint clean up

--- src/game_grid.py
+++ src/game_grid.py
@@ -20,7 +20,8 @@ class GameGrid:
         return grid
 
     def is_valid_move(self, block, placed_blocks):
-        valid_moves = set((col, row) for row in range(GAME_GRID_ROWS) for col in range(GAME_GRID_COLUMNS))
+        valid_moves = set((col, row) for row in range(GAME_GRID_ROWS) \
+                                     for col in range(GAME_GRID_COLUMNS))
         blocks_on_grid = set(placed_blocks.keys())
         valid_moves -= blocks_on_grid
         block_positions = set(block.shape_to_coordinates())
@@ -35,12 +36,12 @@ class GameGrid:
                     self.grid[col][row] = BACKGROUND_COLORS["light_grey"]
 
     def reset_all_cell_colors(self, placed_blocks):
-            for row in range(self.grid.shape[0]):
-                for col in range(self.grid.shape[1]):
-                    if (row + col) % 2 == 0 and (row, col) not in placed_blocks:
-                        self.grid[row][col] = BACKGROUND_COLORS["dark_grey"]
-                    elif (row, col) not in placed_blocks:
-                        self.grid[row][col] = BACKGROUND_COLORS["light_grey"]
+        for row in range(self.grid.shape[0]):
+            for col in range(self.grid.shape[1]):
+                if (row + col) % 2 == 0 and (row, col) not in placed_blocks:
+                    self.grid[row][col] = BACKGROUND_COLORS["dark_grey"]
+                elif (row, col) not in placed_blocks:
+                    self.grid[row][col] = BACKGROUND_COLORS["light_grey"]
 
     def clear_rows(self, placed_blocks):
         rows_to_clear = []
@@ -48,7 +49,8 @@ class GameGrid:
         cleared = False
 
         for i in range(self.grid.shape[0]):
-            if np.all(self.grid[i] != BACKGROUND_COLORS["dark_grey"]) and np.all(self.grid[i] != BACKGROUND_COLORS["light_grey"]):
+            if np.all(self.grid[i] != BACKGROUND_COLORS["dark_grey"]) \
+               and np.all(self.grid[i] != BACKGROUND_COLORS["light_grey"]):
                 rows_to_clear.append(i)
 
         rows_cleared = len(rows_to_clear) # think what to do with this
@@ -57,10 +59,7 @@ class GameGrid:
             for row in rows_to_clear:
                 self.grid[1:row+1] = self.grid[0:row].copy()
                 for col in range(self.columns):
-                    try:
-                        del placed_blocks[(col, row)]
-                    except:
-                        continue
+                    placed_blocks.pop((col, row), None)
 
             for row, col in sorted(placed_blocks, key=lambda coord: coord[1], reverse=True):
                 if col > rows_cleared:
